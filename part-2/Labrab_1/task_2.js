f096d6f77095ac3e5259a4e7478fc95689d958c4
Dix1k <148674528+Dix1k@users.noreply.github.com>
2026-03-11 12:50:19 +0500

    fix task_2

--- part-2/Labrab_1/task_2.js
+++ part-2/Labrab_1/task_2.js
@@ -4,27 +4,22 @@ let json = `
         "b":   { "c": 2, "d": 3 }, 
         "e": 4, 
         "fff":{ "v": 10 } 
-    };`;
+    };`
 
 // Регулярное выражение
-const regex = /"([^"]+)"\s*:\s*(\{[^{}]*\})/g;
+const regex = /"([^"]+)"\s*:\s*(\{[^{}]*\})/g
 
 // Для хранения результатов
-const objectValues = [];
-const fieldNames = [];
-const pairs = [];
+const objectValues = []
+const fieldNames = []
+const pairs = []
 
-let match;
-while ((match = regex.exec(json)) !== null) {
-    const fieldName = match[1];
-    const objectValue = match[2];
-    
-    fieldNames.push(fieldName);
-    objectValues.push(objectValue);
-    pairs.push([fieldName, objectValue]);
-}
+const match = (json, regex) =>
+	[...json.matchAll(regex)].map(([, fieldName, objectValue]) => ({
+		fieldName,
+		objectValue,
+		pairs: [fieldName, objectValue]
+	}))
 
 // Вывод результатов
-console.log(objectValues);
-console.log(fieldNames);
-console.log(pairs);
+console.log(match(json, regex))
