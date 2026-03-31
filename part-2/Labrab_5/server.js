777b14a0c009c11814ee461403a0269110ef7af4
Dix1k <148674528+Dix1k@users.noreply.github.com>
2026-03-31 13:30:21 +0500

    lab_5 0.2 beta

--- part-2/Labrab_5/server.js
+++ part-2/Labrab_5/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const log = console.log;
+const users = require("./users.json");
+const book = require("./book.json");
 
 const HOST = "localhost",
   PORT = 3000;
